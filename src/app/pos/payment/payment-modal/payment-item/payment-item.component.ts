81f93bca73d36a9e85e4ece940f3b14f9ad908c5
Fallenzone <pingfallenzone@gmail.com>
2021-04-09 17:56:41 +0900

    update payment mechiscm

--- src/app/pos/payment/payment-modal/payment-item/payment-item.component.ts
+++ src/app/pos/payment/payment-modal/payment-item/payment-item.component.ts
@@ -1,5 +1,5 @@
 import { PaymentService } from './../../payment.service';
-import { Component, ElementRef, EventEmitter, Input, OnInit, Output, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output, ViewChild, AfterViewInit } from '@angular/core';
 import { ModalController, AlertController } from '@ionic/angular';
 import { PosService } from 'src/app/pos/pos.service';
 import { PaymentRemitComponent } from '../payment-remit/payment-remit.component';
@@ -20,16 +20,20 @@ export class PaymentItemComponent implements OnInit, AfterViewInit {
   methodName = '現金';
   totalPrice: number;
   layaway = 1;
-  remit: { code: string, name: string };
+  layawayBank = '';
+  remitInfo: { code: string, name: string, digi: string };
+  cardInfo: { authCode: string, digi: string, payInfo: { times: number, bank: string } };
   isConfirmDisabled = false;
   payPrice: number;
   authCodeLength: number;
   paymentList: { name: string, payPrice: number, info: {} }[];
   btnNewPaymentDisabled = false;
   info = {};
-  @ViewChild('inputAuthCode', { read: ElementRef }) private authCodeEl: ElementRef;
-  @ViewChild('inputCredit4Digi', { read: ElementRef }) private Credit4DigiEl: ElementRef;
-  @ViewChild('inputRemit5Digi', { read: ElementRef }) private remit5DigiEl: ElementRef;
+
+  remit5Digi = '';
+  authCode = '';
+  card4Digi = '';
+
 
   constructor(private posService: PosService,
     private modalController: ModalController,
@@ -37,6 +41,8 @@ export class PaymentItemComponent implements OnInit, AfterViewInit {
     private alertController: AlertController) { }
 
   ngOnInit() {
+
+    console.log('init payment : ' + JSON.stringify(this.payment));
     // get total price first
     this.totalPrice = this.posService.getTotalPrice();
     // 剛開始是 cash -> true
@@ -47,6 +53,39 @@ export class PaymentItemComponent implements OnInit, AfterViewInit {
     });
 
     this.payPrice = this.payment.payPrice;
+
+
+    const info = this.payment.info;
+    // handle payment select
+    if (this.payment.name === 'remit') {
+      console.log('is remit');
+      if (info) {
+        console.log('has remit info : ', info, info['code'], info['name']);
+        this.remitInfo = { code: info['code'], name: info['name'], digi: info['digi'] };
+        this.remit5Digi = info['digi'];
+
+      } else {
+
+      }
+    } else if (this.payment.name === 'credit-card') {
+      if (this.payment.info) {
+        console.log('payment info ', this.payment.info)
+        this.cardInfo = {
+          authCode: info['authCode'], digi: info['digi'], payInfo: {
+            times: info['payInfo']['times'],
+            bank: info['payInfo']['bank']
+          }
+        };
+        this.authCode = info['authCode'] ? info['authCode'] : '';
+        this.card4Digi = info['digi'] ? info['digi'] : info['digi'];
+        this.layaway = this.cardInfo.payInfo.times;
+      }
+
+
+      console.log('credit card info ', this.cardInfo);
+    }
+
+
 
 
   }
@@ -118,12 +157,28 @@ export class PaymentItemComponent implements OnInit, AfterViewInit {
   }
 
   onLayawaySelect(value) {
-    this.layaway = +value;
+    this.layaway = value;
+
+    this.cardInfo = {
+      authCode: this.authCode.toString(),
+      digi: this.card4Digi.toString(),
+      payInfo: {
+        times: this.layaway,
+        bank: this.layawayBank
+      }
+    };
+    this.updateInfo(this.cardInfo);
   }
 
+  updateInfo(value) {
+    this.info = value;
+    this.payment.info = this.info;
+    this.paymentService.editPayment(this.payment);
+  }
 
   // 如果已選了匯款資訊，但要更改匯款銀行時
   onRemitItemClick() {
+    console.log('renit click ! ');
     this.onRemitSelect();
   }
 
@@ -138,8 +193,16 @@ export class PaymentItemComponent implements OnInit, AfterViewInit {
 
     modal.onWillDismiss().then(result => {
       const data = result.data;
-      console.log('close modal , bank : ' + data);
-      this.remit = data;
+      console.log('close modal , bank : ' + JSON.stringify(data));
+      this.remitInfo = { code: data.code, name: data.name, digi: '' };
+      // this.remitInfo.name = data.name;
+
+
+      // add payment info
+      this.payment.info = this.remitInfo;
+
+      // update payment info to payment list
+      this.paymentService.editPayment(this.payment);
     });
 
     return await modal.present();
@@ -152,9 +215,18 @@ export class PaymentItemComponent implements OnInit, AfterViewInit {
   }
 
   onRemitInfoChange() {
-    const remitCode = this.remit5DigiEl.nativeElement.value;
+    const remitCode = this.remit5Digi.toString();
     if (remitCode.length === 5) {
       this.isConfirmDisabled = false;
+
+      // assign code to remitinfo
+      this.remitInfo.digi = remitCode;
+
+      // assign to payment info
+      this.payment.info = this.remitInfo;
+
+      // update payment list
+      this.paymentService.editPayment(this.payment);
     } else {
       this.isConfirmDisabled = true;
     }
@@ -162,21 +234,42 @@ export class PaymentItemComponent implements OnInit, AfterViewInit {
 
 
   onCreditCardInfoChange() {
-    const authCode = this.authCodeEl.nativeElement.value;
-
+    console.log('credit card changed ');
+    // console.log(this.authCode.toString());
+    // const code = this.authCode.toString();
 
     if (this.payMethod === 'line-pay') {
-      if (authCode.length === this.authCodeLength) {
+      if (this.authCode.toString().length === this.authCodeLength) {
         this.isConfirmDisabled = false;
+        this.info = { authCode: this.authCode };
+        this.payment.info = this.info;
+
+        this.paymentService.editPayment(this.payment);
       } else {
         this.isConfirmDisabled = true;
       }
     } else {
-      const cardDigi = this.Credit4DigiEl.nativeElement.value;
+      // const cardDigi = this.card4Digi.toString();
 
       // credit card
-      if (authCode.length === this.authCodeLength && cardDigi.length === 4) {
+      console.log(this.authCode.toString().length);
+      if (this.authCode.toString().length === this.authCodeLength && this.card4Digi.toString().length === 4) {
         this.isConfirmDisabled = false;
+
+
+        this.cardInfo = {
+          authCode: this.authCode.toString(),
+          digi: this.card4Digi.toString(),
+          payInfo: {
+            times: this.layaway,
+            bank: this.layawayBank
+          }
+        };
+
+        this.info = this.cardInfo;
+        this.payment.info = this.info;
+        this.paymentService.editPayment(this.payment);
+
       } else {
         this.isConfirmDisabled = true;
       }
